3088aa8ab26b8ffe755fb161a944e488284e2b91
stmmala <stmmala1@memphis.edu>
2020-02-05 11:33:47 -0600

    Initial commit

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -2,7 +2,7 @@ class StaticPagesController < ApplicationController
   def welcome
     
     respond_to do |format|
-      format.html { render :welcome }
+     format.html { render :welcome }
     end
   end
   def about
